8d38bb46d8eedd896a8e1f8fb8edb10e4d805dd1
Anna Siwik <annamagdasiwik@gmail.com>
2017-03-19 18:36:38 +0100

    Individuals crossover

--- src/solution/GeneticAlgorithm.java
+++ src/solution/GeneticAlgorithm.java
@@ -4,6 +4,7 @@ import msrcpsp.evaluation.DurationEvaluator;
 import msrcpsp.io.MSRCPSPIO;
 import msrcpsp.scheduling.*;
 import msrcpsp.scheduling.greedy.Greedy;
+import msrcpsp.validation.CompleteValidator;
 
 import java.util.*;
 
@@ -18,22 +19,27 @@ public class GeneticAlgorithm
     private BaseIntIndividual[] population;
     private int populationSize;
     private int contestSize;
+    private double crossoverProbability;
+    private double mutationProbability;
 
     public static void main(String[] args)
     {
         MSRCPSPIO reader = new MSRCPSPIO();
         Schedule s = reader.readDefinition("src\\resources\\def_small\\10_3_5_3.def");
-        GeneticAlgorithm ga = new GeneticAlgorithm(s, 1, 100, 5);
+        GeneticAlgorithm ga = new GeneticAlgorithm(s, 1, 100, 5, 0.75, 0.1);
         ga.start();
     }
 
-    private GeneticAlgorithm(Schedule schedule, int maxNumberOfGenerations, int populationSize, int contestSize)
+    private GeneticAlgorithm(Schedule schedule, int maxNumberOfGenerations, int populationSize, int contestSize,
+                             double crossoverProbability, double mutationProbability)
     {
         this.schedule = schedule;
         this.maxNumberOfGenerations = maxNumberOfGenerations;
         this.greedy = new Greedy(schedule.getSuccesors());
         this.populationSize = populationSize;
         this.contestSize = contestSize;
+        this.crossoverProbability = crossoverProbability;
+        this.mutationProbability = mutationProbability;
     }
 
     private boolean start()
@@ -44,12 +50,19 @@ public class GeneticAlgorithm
         while (currentGeneration <= maxNumberOfGenerations)
         {
             ArrayList<BaseIntIndividual> selectedIndividuals = selectIndividuals();
-            performCrossover();
+            System.out.println(getTheBestIndividual(selectedIndividuals).getDuration());
+            ArrayList<BaseIntIndividual> crossedIndividuals = performCrossover(selectedIndividuals);
+            System.out.println(getTheBestIndividual(crossedIndividuals).getDuration());
             performMutation();
             ++currentGeneration;
         }
 
         return true;
+    }
+
+    private BaseIntIndividual getTheBestIndividual(ArrayList<BaseIntIndividual> individuals)
+    {
+        return individuals.stream().max(Comparator.naturalOrder()).get();
     }
 
     private BaseIntIndividual[] generateRandomPopulation()
@@ -119,11 +132,52 @@ public class GeneticAlgorithm
         return individuals;
     }
 
-    private void performCrossover()
+    private ArrayList<BaseIntIndividual> performCrossover(ArrayList<BaseIntIndividual> selectedIndividuals)
     {
+        ArrayList<BaseIntIndividual> crossedIndividuals = new ArrayList<>();
+        Random r = new Random();
 
+        while (crossedIndividuals.size() < populationSize)
+        {
+            BaseIntIndividual individual1 = selectedIndividuals.get(r.nextInt(populationSize));
+            BaseIntIndividual individual2 = selectedIndividuals.get(r.nextInt(populationSize));
+
+            if (r.nextDouble() <= crossoverProbability)
+                crossedIndividuals.add(crossIndividuals(individual1, individual2));
+        }
+
+        return crossedIndividuals;
     }
 
+    private BaseIntIndividual crossIndividuals(BaseIntIndividual individual1, BaseIntIndividual individual2)
+    {
+        int[] genes1 = individual1.getGenes();
+        int[] genes2 = individual2.getGenes();
+
+        Random r = new Random();
+        int slicePoint = r.nextInt(genes1.length - 1);
+
+        int[] firstSlice = Arrays.copyOfRange(genes1, 0, slicePoint);
+        int[] secondSlice = Arrays.copyOfRange(genes2, slicePoint, genes1.length);
+        int[] crossedGenes = new int[genes1.length];
+        System.arraycopy(firstSlice, 0, crossedGenes, 0, firstSlice.length);
+        System.arraycopy(secondSlice, 0, crossedGenes, firstSlice.length, secondSlice.length);
+
+        Schedule s = new Schedule(schedule);
+
+        Task[] tasks = s.getTasks();
+        for(int index = 0; index < tasks.length; ++index)
+            s.assign(tasks[index], s.getResource(crossedGenes[index]));
+
+        Greedy tempGreedy = new Greedy(s.getSuccesors());
+        tempGreedy.buildTimestamps(s);
+
+        BaseIntIndividual individual = new BaseIntIndividual(s, crossedGenes, new DurationEvaluator(s));
+        individual.setDurationAndCost();
+
+        return individual;
+    }
+    
     private void performMutation()
     {
 
